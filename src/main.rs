a5a0b373b29c3035b7bee4246149bce557c9e6f4
MedovTimur <medov.tr@phystech.edu>
2024-07-18 18:42:07 +0300

    check generate installed

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,27 @@ fn main() {
         std::process::exit(1);
     }
 
+    let generate_installed = Command::new("cargo")
+        .arg("generate")
+        .arg("--version")
+        .output()
+        .map(|output| output.status.success())
+        .unwrap_or(false);
+
+    if !generate_installed {
+        println!("cargo-generate not found, installing...");
+        let status = Command::new("cargo")
+            .arg("install")
+            .arg("cargo-generate")
+            .status()
+            .expect("failed to install cargo-generate");
+
+        if !status.success() {
+            eprintln!("failed to install cargo-generate");
+            std::process::exit(1);
+        }
+    }
+
     let project_name = &args[2];
     let template_repo = "https://github.com/MedovTimur/template";
 
@@ -24,3 +45,4 @@ fn main() {
         std::process::exit(1);
     }
 }
+
